e29f782be6de323ded608eea0f61753be410aed4
Tongtong Liu <tongtong.liu@salesforce.com>
2019-08-15 14:30:58 -0700

    EP7

--- SlideOutMenu/ViewController/HomeViewController.swift
+++ SlideOutMenu/ViewController/HomeViewController.swift
@@ -9,39 +9,28 @@
 import UIKit
 
 class HomeViewController: UITableViewController {
-
-    fileprivate func setupNavigationItems() {
-        navigationItem.title = "Home"
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .done, target: self, action: #selector(handleHide))
-    }
+    let window = UIApplication.shared.keyWindow
+    let menuViewController = MenuViewController()
+    let maxWidth: CGFloat = 300
+    var isMenuOpen: Bool = false
+    let velocityThreshhold: CGFloat = 500
     
-    fileprivate func setupMenuView() {
-        menuViewController.view.frame = CGRect(x: -maxWidth, y: 0, width: 300, height: view.frame.height)
-        let mainWindow = UIApplication.shared.keyWindow
-        mainWindow?.addSubview(menuViewController.view)
-        addChild(menuViewController)
-    }
+    lazy var darkOverlay: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        view.alpha = 0
+        view.isUserInteractionEnabled = false
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationItems()
         tableView.backgroundColor = .red
         setupMenuView()
-        
-        let gesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-        view.addGestureRecognizer(gesture)
-    }
-    
-    let menuViewController = MenuViewController()
-    let maxWidth: CGFloat = 300
-    var isMenuOpen: Bool = false
-    let velocityThreshhold: CGFloat = 500
-    fileprivate func performAnimation(transform: CGAffineTransform) {
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            self.navigationController?.view.transform = transform
-            self.menuViewController.view.transform = transform
-        })
+        setupPanGesture()
+        window?.addSubview(darkOverlay)
+        darkOverlay.frame = window?.bounds ?? .zero
     }
     
     @objc func handlePan(gesture: UIPanGestureRecognizer) {
@@ -62,34 +51,14 @@ class HomeViewController: UITableViewController {
             let transform = CGAffineTransform(translationX: x, y: 0)
             navigationController?.view.transform = transform
             menuViewController.view.transform = transform
+            darkOverlay.transform = transform
+            darkOverlay.alpha = x/maxWidth
         case .ended:
-            if isMenuOpen {
-                if abs(velocity.x) > velocityThreshhold {
-                    handleHide()
-                    return
-                }
-                
-                if abs(trans.x) > 150 {
-                    handleHide()
-                } else {
-                    handleOpen()
-                }
-            } else {
-                if velocity.x > velocityThreshhold {
-                    handleOpen()
-                    return
-                }
-                
-                if trans.x > 150 {
-                    handleOpen()
-                } else {
-                    handleHide()
-                }
-            }
+            handleEnded(velocity, trans)
         default:
             return
         }
-    
+        
     }
     
     @objc func handleOpen() {
@@ -100,6 +69,60 @@ class HomeViewController: UITableViewController {
     @objc func handleHide() {
         isMenuOpen = false
         performAnimation(transform: .identity)
+    }
+    
+    // MARK: fileprivate functions
+    fileprivate func setupPanGesture() {
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        view.addGestureRecognizer(gesture)
+    }
+    
+    fileprivate func setupNavigationItems() {
+        navigationItem.title = "Home"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .done, target: self, action: #selector(handleHide))
+    }
+    
+    fileprivate func setupMenuView() {
+        menuViewController.view.frame = CGRect(x: -maxWidth, y: 0, width: 300, height: view.frame.height)
+        let mainWindow = UIApplication.shared.keyWindow
+        mainWindow?.addSubview(menuViewController.view)
+        addChild(menuViewController)
+    }
+    
+    fileprivate func performAnimation(transform: CGAffineTransform) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.navigationController?.view.transform = transform
+            self.menuViewController.view.transform = transform
+            self.darkOverlay.transform = transform
+            self.darkOverlay.alpha = transform.isIdentity ? 0 : 1
+        })
+    }
+    
+    fileprivate func handleEnded(_ velocity: CGPoint, _ trans: CGPoint) {
+        if isMenuOpen {
+            if abs(velocity.x) > velocityThreshhold {
+                handleHide()
+                return
+            }
+            
+            if abs(trans.x) > 150 {
+                handleHide()
+            } else {
+                handleOpen()
+            }
+        } else {
+            if velocity.x > velocityThreshhold {
+                handleOpen()
+                return
+            }
+            
+            if trans.x > 150 {
+                handleOpen()
+            } else {
+                handleHide()
+            }
+        }
     }
 }
 
