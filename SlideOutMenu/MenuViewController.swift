cb043d57c2664b49958ff347373349f030e9a61e
Tongtong Liu <tongtong.liu@salesforce.com>
2019-08-14 17:15:00 -0700

    create basic vc - EP1&2

---
+++ SlideOutMenu/MenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.swift
+//  SlideOutMenu
+//
+//  Created by Tongtong Liu on 8/14/19.
+//  Copyright © 2019 Tongtong Liu. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = .yellow
+    }
+}
--- SlideOutMenu/ViewController.swift
+++ SlideOutMenu/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    fileprivate func setupNavigationItems() {
+        navigationItem.title = "Home"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .done, target: self, action: #selector(handleHide))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupNavigationItems()
+        tableView.backgroundColor = .red
     }
-
-
+    
+    @objc func handleOpen() {
+        let vc = MenuViewController()
+        
+        vc.view.frame = CGRect(x: 0, y: 0, width: 300, height: view.frame.height)
+        let mainWindow = UIApplication.shared.keyWindow
+        mainWindow?.addSubview(vc.view)
+    }
+    
+    @objc func handleHide() {
+        
+    }
+    
+    
 }
 
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cellID")
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+}
